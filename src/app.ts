edeaa4255b442efb85f1e678139823c9803cf672
Dipto Dey <diptodey2005@gmail.com>
2023-06-29 20:07:13 +0600

    eslint and prettier utilized

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app : Application = express()
-const port = 3000
+const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+// for testing
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working successfully')
 })
 
-export default app  
+export default app
