6d574d0117f7fca78a588dd3b9d9ddd036ab1341
Dipto Dey <diptodey2005@gmail.com>
2023-07-05 23:47:36 +0600

    create user route, controller then test utility function via postman

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from './config'
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log("Database connected succesfully");
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connected succesfully')
 
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-          })
-        
-    } catch (error) {
-        console.log("failed to connect");
-        
-    }
-  
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('failed to connect')
+  }
 }
 
 main()
