db94957187c7538ebca64174a231898b28971d96
Dipto Dey <diptodey2005@gmail.com>
2023-07-07 20:49:44 +0600

    lint-stage

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { logger, errorLogger } from './shared/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database connected succesfully')
+    logger.info('Database connected succesfully')
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      logger.info(`Example app listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log('failed to connect')
+    errorLogger.error('failed to connect')
   }
 }
 
